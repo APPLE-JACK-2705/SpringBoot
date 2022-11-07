f4bf06f955a9bab88428b79784fed081fab71c63
Adil Adilkhanov <020527550543@stud.satbayev.university>
2022-11-08 01:40:33 +0600

    Spring Boot

---
+++ src/main/java/com/example/springboot/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.springboot.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.springboot")
+public class AppConfig {
+}
